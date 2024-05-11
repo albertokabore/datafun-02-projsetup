3f0f5da6f85dca6ef26db0617f05aec26f14ff31
Albert Kabore <129116752+albertokabore@users.noreply.github.com>
2024-05-10 22:29:12 -0400

    Uploaded python file from VSC

--- AlbertKabore_projsetup.py
+++ AlbertKabore_projsetup.py
@@ -1,1 +1,21 @@
-AlbertKabore_projsetup.py
+""" Start a data analytics project. """
+
+import pathlib
+
+
+def create_project_directory(directory_nam: str): # add type hinting to params
+    """
+    Create a project sub-directory.
+    : param directory_name: Name to be creatd, e,g, "test"
+
+    """
+    pathlib.Path(directory_name).mkdir(exist_ok=True)
+
+def main():
+    """ Scaffold a project. """
+    create_project_directory(dictory_name='test') # name the parameter
+    create_projet_directory(directory_name='docs') # name the parameter
+
+
+    if __name__ == '__main__':
+        main()
